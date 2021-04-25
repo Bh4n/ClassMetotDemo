4e99924d5e18c30aa74d50e09b3fc3326d01e268
Bh4n <Bh4n@users.noreply.github.com>
2021-04-25 21:34:31 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirsName+" "+customer.LastName +" eklendi");
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Id: "+customer.Id+"\nİsim: "+customer.FirsName + "\nSoyad: " + customer.LastName + "\nAdres: " + customer.Address);
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.FirsName + " " + customer.LastName + " güncellendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirsName + " " + customer.LastName + " silindi");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirsName = "Bahan";
+            customer1.LastName = "Morova";
+            customer1.Address = "Ankara_TR";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 1;
+            customer2.FirsName = "Enes";
+            customer2.LastName = "Bahan";
+            customer2.Address = "Malatya_TR";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.List(customer2);
+            customerManager.Delete(customer1);
+        }
+    }
+}
